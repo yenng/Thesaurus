cce72ecb80f1a7b5d9f9448bb6a63ef3e00f722b
Ng Yen Aeng <william951721@gmail.com>
2017-05-18 12:32:19 +0800

    Scroll working

--- ShowNearbyWordsUsingTreeview/treeTest.py
+++ ShowNearbyWordsUsingTreeview/treeTest.py
@@ -11,6 +11,8 @@ from kivy.properties import ObjectProperty
 from kivy.uix.scrollview import ScrollView
 from kivy.core.window import Window
 from kivy.base import runTouchApp, stopTouchApp
+from kivy.uix.textinput import TextInput
+from kivy.graphics import Color
 import dictionary
 
 
@@ -46,7 +48,14 @@ class MyTreeView(TreeView):
     def on_touch_down(self, touch):
         node = self.get_node_at_pos(touch.pos)
         global word
-        word = node.text
+        global tv
+        # This code is to delete the unwanted query word
+        if(touch.button=='right'):
+            # Only remove the node that contains "Query Word:"
+            if node.text.find('Query Word:')==0:
+                tv.remove_node(node)
+        if node != None:
+            word = node.text
         return super(MyTreeView, self).on_touch_down(touch)
         
     
@@ -54,11 +63,11 @@ class MyWidget(Widget):
     def new(self):
         global word
         global tv
-        TestApp().getChildNear(tv, word)
+        TestApp().getChildNear(tv, word)##############
 
 class TestApp(App):
     def build(self):
-        return root
+        return 
 
     def getNear(self,queryWord):
         run = dictionary.Dictionary()
@@ -72,18 +81,18 @@ class TestApp(App):
         #print node.children
         return wordList
 
-
+                        #tv,        
     def buildTree(self, treeOrigin, node, name):
         add = treeOrigin.add_node
         self.root = []
         l = []
         for x in range(len(node.children)):
-            self.root.append(add(TreeViewLabel(text=node.children[x].data, height = 40,is_open=True,
+            self.root.append(add(TreeViewLabel(text=node.children[x].data, height = 40,
                                                size_hint_y=None), name))
             l.append(TestApp().getNear(node.children[x].data))
             nodeT = l[x].firstNode
             for y in range(len(nodeT.children)):
-                add(TreeViewLabel(text=nodeT.children[y].data, size_hint_y=None,is_open=True),self.root[x])
+                add(TreeViewLabel(text=nodeT.children[y].data, size_hint_y=None),self.root[x])
 
     def getChildNear(self, treeOrigin,queryWord):
         wordList = TestApp().getNear(queryWord)
@@ -100,15 +109,20 @@ class TestApp(App):
         #node.printCurrentNode()
 
 word = raw_input('Enter the word: ')
-window = ScrollView(size_hunt=(1,None), size = (Window.width,Window.height))
-root = BoxLayout(orientation='vertical',height = 100)
-root1 = BoxLayout(orientation='horizontal')
-button = Button(text='Generate New List',size_hint=(.1,.1), on_press=MyWidget.new)
-tv = MyTreeView(hide_root=True)
+tvPanel = ScrollView(size_hint=(1,None),Color=(0,1,0,1))
+tvBox = BoxLayout(orientation='vertical',valign='top',height=100)
+boxPanel = BoxLayout(orientation='vertical',valign='top')
+butPanel = BoxLayout(orientation='horizontal',size_hint=(1,.1))
+button = Button(text='Generate New List', on_press=MyWidget.new)
+#button = Button(text='Generate New List',size_hint=(.1,.1), on_press=MyWidget.new)
+tv = MyTreeView(root_options=dict(text='Nearby Words'),hide_root=True)
+tv.size_hint = 1,None
+tv.bind(minimum_height = tv.setter('height'))
 TestApp().getChildNear(tv, word)
-root1.add_widget(button)
-#root1.add_widget(button1)
-root.add_widget(tv)
-root.add_widget(root1)
-window.add_widget(root)
-runTouchApp(window)
+butPanel.add_widget(button)
+tvPanel.add_widget(tv)
+#boxPanel.add_widget(butPanel)
+tvBox.add_widget(tvPanel)
+boxPanel.add_widget(tvBox)
+boxPanel.add_widget(butPanel)
+runTouchApp(boxPanel)
