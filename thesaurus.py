557c214743bc3a1a0958a68bd3c155f86ba9d8d5
Ng Yen Aeng <william951721@gmail.com>
2017-05-20 00:04:57 +0800

    Update thesaurus.py

--- thesaurus.py
+++ thesaurus.py
@@ -1,17 +1,17 @@
 from bs4 import BeautifulSoup
 import urllib
 
-# Get the nearby words from www.dictionary.com
+# Get the nearby words from www.thesaurus.com
 class Thesaurus:
   def getNearbyWord(self,queryWord):
     count = 10
-    # Create an array to store all nearby words
+    # Create an array to store all synonyms
     nearbyList = []
     
     # Replace all ' ' with '-' in the query word
     queryWord = queryWord.replace(" ", "-")
 
-    # Create the link that connect to www.dictionary.com with given query word
+    # Create the link that connect to www.thesaurus.com with given query word
     self.link = "http://www.thesaurus.com/browse/" + queryWord
 
     # Open the url and read the html
@@ -20,11 +20,11 @@ class Thesaurus:
     
     # Pass html using beautifulsoup
     soup = BeautifulSoup(html,'html.parser')
-    for sectionClass in soup.find_all('div'):
-      # Travese the html parse tree to nearby words section
-      if(sectionClass.get('class')==[u'relevancy-list']):
+    for divClass in soup.find_all('div'):
+      # Travese the html parse tree to relevancy-list section
+      if(divClass.get('class')==[u'relevancy-list']):
         # Extract all nearby words.
-        for nearby in sectionClass.find_all('li'):
+        for nearby in divClass.find_all('li'):
           if count > 0:
             if(nearby.a.string != queryWord):
               nearbyList.append(nearby.span.string)
